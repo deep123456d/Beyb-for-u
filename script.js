51c3b6294afe16a755151481a6dc1234340e43fb
deep123456d <85383233+deep123456d@users.noreply.github.com>
2026-07-31 23:11:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const envelope = document.getElementById("envelope");
+const button = document.getElementById("openBtn");
 
+button.addEventListener("click", function(e){
+
+    e.stopPropagation();
+
+    envelope.classList.toggle("open");
+
+    if(button.innerText==="Open My Surprise 💌"){
+
+        button.innerText="Close Envelope 💜";
+
+    }else{
+
+        button.innerText="Open My Surprise 💌";
+
+    }
+
+});
